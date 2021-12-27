9281b18b052e5f33c78e19964cb6a3872fea3534
PirateTosh <srs.paritosh7@gmail.com>
2021-12-27 23:17:58 +0530

    Project files

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+// export const EventId = "cQfC27MHoMqbzNpNw";
+// export const AppTItle = "VisionIT 2020 Conference";
+// export const eventTitle = "VisionIT 2020 Conference";
+// export const AppShortName = "SoftechLife";
+// export const AppURL = "https://softechlife.thekeyevent.in";
+// export const isMobile = window.innerWidth <= 500;
+
+
+export const EventId = "WqRnyqjPvx4ot7ins";
+export const AppTItle = "K & A WEDDING";
+export const eventTitle = "K & A WEDDING";
+export const AppShortName = "K & A WEDDING";
+export const AppURL = "";
+export const isMobile = window.innerWidth <= 500;
+
