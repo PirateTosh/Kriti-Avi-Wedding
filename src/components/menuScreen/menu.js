bb63f44a04eef21d877d655bec3be89c3e44c5b8
PirateTosh <srs.paritosh7@gmail.com>
2021-12-30 19:52:16 +0530

    fixes

--- src/components/menuScreen/menu.js
+++ src/components/menuScreen/menu.js
@@ -183,7 +183,7 @@ class Menu extends React.Component {
 		return (
 			<div className="categories">
 								<img id="homeLogo" src={testimage} alt="" style={{width:"130px", height:"130px", marginTop:"10%"}}/>
-								<img id="homeLogoMob" src={testimage} alt="" style={{ display:"none",width:"95px", height:"95px", marginTop:"27%", marginBottom:"2%"}}/>
+								<img id="homeLogoMob" src={testimage} alt="" style={{ display:"none",width:"95px", height:"95px", marginTop:"24%", marginBottom:"2%"}}/>
 
 								<h2 id="tag" style={{display: "block", color:"#781800ff", fontSize:"15px", marginTop:"1%",marginBottom:"-19%",fontFamily:"Scope one regular",fontWeight:"bold"}}>#KASealedTheDeal</h2>
 								<h2 id="tagMob" style={{display: "none", color:"#781800ff", fontSize:"15px", marginBottom:"-40%",fontFamily:"Scope one regular",fontWeight:"bold"}}>#KASealedTheDeal</h2>
